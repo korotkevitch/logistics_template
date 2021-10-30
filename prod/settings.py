73744413868d711d2adf55205fa94bee9fd57ee5
Igor Korotkevitch <i.korotkevitc@gmail.com>
2021-10-30 13:30:02 +0300

    Removed 127.0.0.1

--- prod/settings.py
+++ prod/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'litstudio.ru', 'www.litstudio.ru']
+ALLOWED_HOSTS = ['litstudio.ru', 'www.litstudio.ru']
 
 
 # Application definition
